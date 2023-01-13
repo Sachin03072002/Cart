853004ffb895a2193e4615e9bc3c758a906f595e
Sachin <sachinmondal720@gmail.com>
2023-01-13 16:55:27 +0530

    add increasQuantity func

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,20 +2,43 @@ import React from "react";
 
 
 class CartItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+        //we can also bind the function here and call its refernce in the onClick function
+        //this.increaseQuantity = this.increaseQuantity.bind(this);
+
+    }
+    //or use arrow function for binding
+    increaseQuantity = () => {
+        console.log('state',this.state);
+        
+    }
     render(){
+        const { price,title,qty }=this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} />
                 </div>
                 <div className="right-block">
-                    <div style={ { fontSize:25} }>Phone</div>
-                    <div style={ { color:'#777'} }>Rs 999</div>
-                    <div style={ { color:'#777'} }>Qty: 1</div>
+                    <div style={ { fontSize:25} }>{title}</div>
+                    <div style={ { color:'#777'} }>Rs {price}</div>
+                    <div style={ { color:'#777'} }>Qty: {qty}</div>
                     <div className="cart-item-actions">
-                        {
-                            // buttons
-                        }
+                        
+                            
+                            <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/3524/3524388.png" onClick={this.increaseQuantity.bind(this)} />
+                            <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/56/56889.png" />
+                            <img alt="delete" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png" />
+                            
+                        
+                        
                     </div>
                 </div>
 
